8669cc39ba86b2c4162e17569fedc192aa35a019
hmdgms <hamdigumus@tutamail.com>
2020-12-13 23:25:58 +0300

    ekran tasarımı kalan dosyalar ve model dosyaları

---
+++ main.py
@@ -0,0 +1,17 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from controllers.login_controller import LoginForm
+from controllers.main_controller import AnaEkran
+import sys
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    app.setStyle('motif')
+    MainWindow = QtWidgets.QMainWindow()
+    ui = AnaEkran()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    login = LoginForm()
+    login.window = MainWindow
+    login.show()
+    sys.exit(app.exec_())
